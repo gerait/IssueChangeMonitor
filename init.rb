6d2c167d8e25cc935ce4307393f8b98419d12702
ghostmiha <ghost.miha@gmail.com>
2012-07-02 13:46:23 +0300

    First commit. Skeleton of plugin

---
+++ init.rb
@@ -0,0 +1,10 @@
+Redmine::Plugin.register :issue_change_monitor do
+  name 'Issue Change Monitor plugin'
+  author 'Mihail Skvirskiy'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+  
+  
+end
